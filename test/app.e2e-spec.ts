abf39d2689a1f8573dbce11c38aace132b291579
Rafael Singer <rafaelsinger66@gmail.com>
2024-01-12 22:33:19 -0800

    added bookmarks crud functionality and e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,8 @@ import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
 import { AuthDto } from "../src/auth/dto";
+import { EditUserDto } from "../src/user/dto";
+import { CreateBookmarkDto, EditBookmarkDto } from "../src/bookmark/dto";
 
 describe('App e2e', () => {
   let app: INestApplication
@@ -112,14 +114,108 @@ describe('App e2e', () => {
           .expectStatus(200)  
       })
     })
-    describe('Edit user', () => {})
+    describe('Edit user', () => {
+      it('should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Rafa',
+          email: 'rafa@yahoo.com'
+        }
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.firstName)  
+          .expectBodyContains(dto.email)  
+      })
+    })
   });
 
   describe('Bookmarks', () => {
-    describe('Create bookmark', () => {})
-    describe('Get bookmarks', () => {})
-    describe('Get bookmark by id', () => {})
-    describe('Edit bookmark', () => {})
-    describe('Delete bookmark', () => {})
+    describe('Get empty bookmarks', () => {
+      it('should get bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .expectStatus(200)
+          .expectBody([])
+      })
+    })
+    describe('Create bookmark', () => {
+      const dto: CreateBookmarkDto = {
+        title: 'First Bookmark',
+        link: 'https://www.google.com'
+      }
+      it('should create bookmark', () => {
+         return pactum
+          .spec()
+          .post('/bookmarks')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .withBody(dto)
+          .expectStatus(201)  
+          .stores('bookmarkId', 'id') 
+      })
+    })
+    describe('Get bookmarks', () => {
+      it('should get bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .expectStatus(200)
+          .expectJsonLength(1)
+      })
+    })
+    describe('Get bookmark by id', () => {
+      it('should get bookmark by id', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .expectStatus(200)
+          .inspect()
+          .expectBodyContains('$S{bookmarkId}')
+      })
+    })
+    describe('Edit bookmark by id', () => {
+      const dto: EditBookmarkDto = {
+        title: 'Google | Find what you need',
+        description: 'Search engine'
+      }
+      it('should edit bookmark by id', () => {
+        return pactum
+          .spec()
+          .patch('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .withBody(dto)
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .expectStatus(200)
+          .inspect()
+          .expectBodyContains(dto.title)
+          .expectBodyContains(dto.description)
+      })
+    })
+    describe('Delete bookmark by id', () => {
+      it('should delete bookmark by id', () => {
+        return pactum
+          .spec()
+          .delete('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .expectStatus(204)
+      })
+
+      it('should get empty bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({Authorization : 'Bearer $S{userAt}'})
+          .expectStatus(200)
+          .expectJsonLength(0)        
+      })
+    })
   });
 })
